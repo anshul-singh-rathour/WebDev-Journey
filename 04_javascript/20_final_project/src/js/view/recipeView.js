0cb163538e8ba2f34cf90931d9f421a559d0c1b9
Anshul Singh Rathour <anshulsinghrathore2020@gmail.com>
2026-08-13 00:12:02 +0530

    done

--- 04_javascript/20_final_project/src/js/view/recipeView.js
+++ 04_javascript/20_final_project/src/js/view/recipeView.js
@@ -64,7 +64,7 @@ class RecipeView extends View {
             </div>
           </div>
 
-          <div class="recipe__user-generated ${this.data.key ? '' : 'hidden'}">
+          <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
             <svg>
               <use href="${icons}.svg#icon-user"></use>
             </svg>
